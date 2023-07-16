bacee8eff48cc03e4fe37028ed7566f3f1345b48
ShevchukAV666 <666mog@mail.ru>
2023-07-16 19:38:57 +0500

    Коммит

---
+++ src/main/java/work/home/home_work_2_12/HomeWork212Application.java
@@ -0,0 +1,13 @@
+package work.home.home_work_2_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork212Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork212Application.class, args);
+	}
+
+}
---
+++ src/test/java/work/home/home_work_2_12/HomeWork212ApplicationTests.java
@@ -0,0 +1,13 @@
+package work.home.home_work_2_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork212ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
